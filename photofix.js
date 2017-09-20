63b4c00e90371421ff6b60ca14ca4c1acc5e9c48
Shaun Starsprung <shaun.starsprung@nike.com>
2017-09-20 16:38:57 -0700

    Fix for Dreamwidth and WordPress proxies

--- photofix.js
+++ photofix.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const URL_PATTERN = new RegExp('^[^:]+://([^.]+\\.)?photobucket.com/.*');
+const PHOTOBUCKET_URL_PATTERN = new RegExp('^[^:]+://([^.]+\\.)?photobucket.com/.*');
 const REFERER = 'http://photobucket.com/gallery/user/';
 
-chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+const PROXY_URL_PATTERN = new RegExp('[^:]+://(?:(?:[^.]+\\.)?wp.com|p.dreamwidth.org)/.*?((?:[^./]+\\.)?photobucket.com/.*)');
+
+chrome.webRequest.onBeforeSendHeaders.addListener(({requestHeaders}) => {
   let refererSet = false;
-  for (const header of details.requestHeaders) {
+  for (const header of requestHeaders) {
     const {name, value} = header;
     if (name === 'Referer') {
-      if (value && !value.match(URL_PATTERN)) {
+      if (value && !value.match(PHOTOBUCKET_URL_PATTERN)) {
         header.value = REFERER;
       }
 
@@ -18,8 +20,26 @@ chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
   };
 
   if (!refererSet) {
-    details.requestHeaders.push({name: 'Referer', value: REFERER});
+    requestHeaders.push({name: 'Referer', value: REFERER});
   }
 
-  return {requestHeaders: details.requestHeaders};
-}, {urls: ['*://*.photobucket.com/*']}, ['blocking', 'requestHeaders']);
+  return {requestHeaders: requestHeaders};
+}, {
+  urls: ['*://*.photobucket.com/*'],
+  types: ['image']
+}, ['blocking', 'requestHeaders']);
+
+chrome.webRequest.onBeforeRequest.addListener(({url}) => {
+  const [, photobucketUrl] = url.match(PROXY_URL_PATTERN);
+  if (photobucketUrl) {
+    return {
+      redirectUrl: `https://${photobucketUrl}`
+    };
+  }
+}, {
+  urls: [
+    '*://p.dreamwidth.org/*photobucket.com/*',
+    '*://*.wp.com/*photobucket.com/*'
+  ],
+  types: ['image']
+}, ['blocking'])
